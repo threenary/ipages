f5c93696794ce1b01e928a2ad6fb021c665898d2
Gonzalo Gomez Sullain gonzalo.gomez@tomtom.com <ggomezsullain@gmail.com>
2019-05-27 21:49:38 +0200

    Updating run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo ======================================================================
+echo
+echo                        ePages Interview Challenge API
+echo
+echo ======================================================================
+echo
+echo Author: Gonzalo Gómez Sullain
+echo Delivery: 27.May.2019
+echo
+echo
+echo Hope you enjoy my solution!!!
+echo
+echo ======================================================================
+
+./gradlew bootRun
